b7d730e208ee99dba5e320bfb0013768ea3b4189
Ari Fajarli Budianto <103985905+AriFajarliBudianto@users.noreply.github.com>
2022-04-21 20:05:37 +0700

    Uts Ari
    Ari fajarli budianto
    191011402585
    06tplm003

---
+++ ari.py
@@ -0,0 +1,8 @@
+#Ari fajarli budianto
+#191011402585
+#06tplm003
+
+nama_Mahasiswa = ['Ari Fajarli Budianto', 'BimaAditya', 'Rivaldy', 'Novia', 'Diyanah']
+
+for Mahasiswa in nama_Mahasiswa:
+	print(Mahasiswa)
